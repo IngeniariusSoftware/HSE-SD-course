9430c9efbe0f3695096020e8ddad731f42b57ad4
IngeniariusSoftware <kyngfy-panda@yandex.ru>
2018-09-27 23:48:03 +0500

    Создано решение для задачи дерева расписаний

---
+++ Tree-identification/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tree_identification
+{
+    using System.Text.RegularExpressions;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+
+        static void SentenceParser(string sentence)
+        {
+            Regex regex = new Regex("\n");
+        }
+        
+    }
+}
