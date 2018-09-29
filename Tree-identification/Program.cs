8755076e475b44a03d721a65c97cf333b5f19f72
IngeniariusSoftware <kyngfy-panda@yandex.ru>
2018-09-29 11:41:12 +0500

    Добавил регексы для парсинга предложения

--- Tree-identification/Program.cs
+++ Tree-identification/Program.cs
@@ -12,13 +12,54 @@ namespace Tree_identification
     {
         static void Main(string[] args)
         {
+            //  "int, const, class, float, string, bool, char, ref, out";
+            //  Enum type = "int, float, string, char, bool";
+            //  Enum object = "class, const, /method, /vars";
+            //  Enum mode = "ref, out /val"; 
+
+            
+            Regex reg = new Regex(@"((?<type>(int)|(char)|(float)|(bool)|(string))|(?<object>(const)|(class)))+\s*(?<name>\S*)+\s*(?<value>^[ =]*)+");
+            string str = "classfrgrgregrdintsfschareffefewfclassefecons";
+            Console.WriteLine(reg.Matches(str)[0].Groups["type"]);
+            Console.WriteLine(reg.Matches(str)[0].Groups["type"]);
+
+            Console.ReadLine();
+
+            if (reg.Matches(str)[0].Groups["type"].ToString() != "")
+            {
+                if (reg.Matches(str)[1].Groups["object"].ToString() != "")
+                {
+                    
+                }
+                else
+                {
+                    
+                }
+            }
+            else
+            {
+                if (reg.Matches(str)[1].Groups["object"].ToString() != "")
+                {
+
+                }
+                else
+                {
+
+                }
+            }
 
         }
 
 
         static void SentenceParser(string sentence)
         {
-            Regex regex = new Regex("\n");
+            //foreach (var VARIABLE in COLLECTION)
+            //{
+
+            //}
+            //Regex regex = new Regex(@"(?<>)");
+
+
         }
         
     }
