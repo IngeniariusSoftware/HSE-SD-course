95485eb4124e059acd34c9e1b5f44cff791c10f5
IngeniariusSoftware <kyngfy-panda@yandex.ru>
2018-09-30 18:36:22 +0500

    Добавил основной функционал

--- Tree-identification/Program.cs
+++ Tree-identification/Program.cs
@@ -1,66 +1,91 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Structures;
 
 namespace Tree_identification
 {
+    using System.IO;
     using System.Text.RegularExpressions;
+    using Program_Elements;
 
     class Program
     {
-        static void Main(string[] args)
+        public static List<(Types.Value, Types.Parameter)> ArgumentsParser(string methodInput)
         {
-            //  "int, const, class, float, string, bool, char, ref, out";
-            //  Enum type = "int, float, string, char, bool";
-            //  Enum object = "class, const, /method, /vars";
-            //  Enum mode = "ref, out /val"; 
-
-            
-            Regex reg = new Regex(@"((?<type>(int)|(char)|(float)|(bool)|(string))|(?<object>(const)|(class)))+\s*(?<name>\S*)+\s*(?<value>^[ =]*)+");
-            string str = "classfrgrgregrdintsfschareffefewfclassefecons";
-            Console.WriteLine(reg.Matches(str)[0].Groups["type"]);
-            Console.WriteLine(reg.Matches(str)[0].Groups["type"]);
-
-            Console.ReadLine();
-
-            if (reg.Matches(str)[0].Groups["type"].ToString() != "")
+            Regex regMethod = new Regex(@"\s*(?<param>(ref)|(out))*\s*(?<value>(int)|(char)|(float)|(bool)|(string))");
+            List<(Types.Value, Types.Parameter)> arguments = new List<(Types.Value, Types.Parameter)>();
+            foreach (Match match in regMethod.Matches(methodInput))
             {
-                if (reg.Matches(str)[1].Groups["object"].ToString() != "")
+                arguments.Add(
+                    (Types.ToValue(match.Groups["value"].ToString()),
+                        Types.ToParameter(match.Groups["param"].ToString())));
+            }
+
+            return arguments;
+        }
+
+        public static Identificator SentenceParser(string sentence)
+        {
+            Regex regSentence = new Regex(
+                @"\s*(?<object>(const)|(class))*\s*(?<type>(int)|(char)|(float)|(bool)|(string))*\s*(?<name>[^ =;()]*)[ =]*(?<value>[^;]*)");
+
+            var matches = regSentence.Matches(sentence)[0];
+            if (matches.Groups["object"].ToString() != string.Empty)
+            {
+                if (matches.Groups["type"].ToString() != string.Empty)
                 {
-                    
+                    return new Const(
+                        matches.Groups["name"].ToString(),
+                        matches.Groups["value"].ToString(),
+                        Types.ToValue(matches.Groups["type"].ToString()));
                 }
                 else
                 {
-                    
+                    return new Class(matches.Groups["name"].ToString());
                 }
             }
             else
             {
-                if (reg.Matches(str)[1].Groups["object"].ToString() != "")
+                if (matches.Groups["value"].ToString() != string.Empty)
                 {
-
+                    return new Method(
+                        matches.Groups["name"].ToString(),
+                        new List(ArgumentsParser(matches.Groups["value"].ToString())),
+                        Types.ToValue(matches.Groups["type"].ToString()));
                 }
                 else
                 {
-
+                    return new Variable(
+                        matches.Groups["name"].ToString(),
+                        Types.ToValue(matches.Groups["type"].ToString()));
                 }
             }
-
         }
 
-
-        static void SentenceParser(string sentence)
+        public static List<string> FileParser(string fileName)
         {
-            //foreach (var VARIABLE in COLLECTION)
-            //{
+            FileStream studentsFile = new FileStream(fileName + ".txt", FileMode.OpenOrCreate);
+            StreamReader reader = new StreamReader(studentsFile);
+            List<string> text = new List<string>();
+            while (!reader.EndOfStream)
+            {
+                text.Add(reader.ReadLine());
+            }
 
-            //}
-            //Regex regex = new Regex(@"(?<>)");
-
-
+            studentsFile.Close();
+            return text;
         }
-        
+
+        static void Main()
+        {
+            BinaryTree tree = new BinaryTree();
+            foreach (string sentence in FileParser("тест"))
+            {
+                tree.Add(SentenceParser(sentence));
+            }
+
+            Console.WriteLine("\n\tДля завершения работы нажмите любую клавишу...");
+            Console.ReadKey();
+        }
     }
 }
