2dee8e1bb8fa3c3d50a1dbb07fbb2eecaceb1d3c
IngeniariusSoftware <kyngfy-panda@yandex.ru>
2018-09-30 18:25:59 +0500

    Создал класс константа, для хранения констант программы

---
+++ Tree-identification/Const.cs
@@ -0,0 +1,17 @@
+﻿namespace Program_Elements
+{
+    class Const : Identificator
+    {
+        private string _value;
+
+        public Const(
+            string name,
+            string value,
+            Types.Value valueType,
+            Types.Identificator identType = Types.Identificator.consts)
+            : base(name, valueType, identType)
+        {
+            _value = value;
+        }
+    }
+}
