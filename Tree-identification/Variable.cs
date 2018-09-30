8a8dfe60b15ad8c620357460e8166953ab391a85
IngeniariusSoftware <kyngfy-panda@yandex.ru>
2018-09-30 18:28:09 +0500

    Создал класс "переменная" для хранения переменных программы

---
+++ Tree-identification/Variable.cs
@@ -0,0 +1,10 @@
+﻿namespace Program_Elements
+{
+    class Variable : Identificator
+    {
+        public Variable(string name, Types.Value valueType, Types.Identificator identType = Types.Identificator.vars)
+            : base(name, valueType, identType)
+        {
+        }
+    }
+}
