747688465322d7d813dbd7a4ecf84838ce892523
IngeniariusSoftware <kyngfy-panda@yandex.ru>
2018-09-30 18:35:46 +0500

    Создал абстрактный класс "идентификатор", как атомарный объект программы

---
+++ Tree-identification/Identificator.cs
@@ -0,0 +1,21 @@
+﻿namespace Program_Elements
+{
+    abstract public class Identificator
+    {
+        public Identificator(string name, Types.Value valueType, Types.Identificator identType)
+        {
+            _name = name;
+            Hash = name.GetHashCode();
+            _valueType = valueType;
+            _identType = identType;
+        }
+
+        private string _name;
+
+        public int Hash;
+
+        private Types.Identificator _identType;
+
+        private Types.Value _valueType;
+    }
+}
