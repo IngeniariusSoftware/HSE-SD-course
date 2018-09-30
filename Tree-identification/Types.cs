fd4da639288d041161ca1b718e4e284e3e38f5a9
IngeniariusSoftware <kyngfy-panda@yandex.ru>
2018-09-30 18:27:47 +0500

    Создал класс типов для хранения различных перечислимых типов программы

---
+++ Tree-identification/Types.cs
@@ -0,0 +1,104 @@
+﻿namespace Program_Elements
+{
+    public class Types
+    {
+        public enum Identificator
+        {
+            classes,
+
+            consts,
+
+            vars,
+
+            methods
+        }
+
+        public enum Value
+        {
+            int_type,
+
+            float_type,
+
+            bool_type,
+
+            char_type,
+
+            string_type,
+
+            class_type,
+
+            void_type
+        }
+
+        public enum Parameter
+        {
+            param_val,
+
+            param_ref,
+
+            param_out
+        }
+
+        public static Value ToValue(string type)
+        {
+            switch (type)
+            {
+                case "int":
+                    {
+                        return Value.int_type;
+                    }
+
+                case "char":
+                    {
+                        return Value.char_type;
+                    }
+
+                case "bool":
+                    {
+                        return Value.bool_type;
+                    }
+
+                case "float":
+                    {
+                        return Value.float_type;
+                    }
+
+                case "class":
+                    {
+                        return Value.class_type;
+                    }
+
+                case "string":
+                    {
+                        return Value.string_type;
+                    }
+                    
+                default:
+                    {
+                        return Value.void_type;
+                    }
+            }
+        }
+
+        public static Parameter ToParameter(string type)
+        {
+            switch (type)
+            {
+                case "ref":
+                    {
+                        return Parameter.param_ref;
+                    }
+
+                case "out":
+                    {
+                        return Parameter.param_out;
+                    }
+
+                default:
+                    {
+                        return Parameter.param_val;
+                    }
+            }
+        }
+    }
+}
