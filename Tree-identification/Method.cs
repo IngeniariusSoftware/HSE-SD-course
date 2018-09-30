4a3b0e607d098ce30f9a36873c680699b4534353
IngeniariusSoftware <kyngfy-panda@yandex.ru>
2018-09-30 18:27:13 +0500

    Создал класс "метод" для хранения методов программы

---
+++ Tree-identification/Method.cs
@@ -0,0 +1,17 @@
+﻿namespace Program_Elements
+{
+    class Method : Identificator
+    {
+        private Structures.List _arguments;
+
+        public Method(
+            string name,
+            Structures.List arguments,
+            Types.Value valueType,
+            Types.Identificator identType = Types.Identificator.methods)
+            : base(name, valueType, identType)
+        {
+            _arguments = arguments;
+        }
+    }
+}
