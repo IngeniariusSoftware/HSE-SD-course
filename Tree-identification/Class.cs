21dff35e7a8dd40230fb000de65ea91c4569a1ee
IngeniariusSoftware <kyngfy-panda@yandex.ru>
2018-09-30 18:26:14 +0500

    Создал класс "класс", для хранения классов программы

---
+++ Tree-identification/Class.cs
@@ -0,0 +1,13 @@
+﻿namespace Program_Elements
+{
+    class Class : Identificator
+    {
+        public Class(
+            string name,
+            Types.Identificator identType = Types.Identificator.classes,
+            Types.Value valueType = Types.Value.class_type)
+            : base(name, valueType, identType)
+        {
+        }
+    }
+}
