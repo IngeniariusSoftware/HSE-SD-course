3cd670b691db056ecffb30b57dd5acf23cf81073
Максим <kyngfy-panda@yandex.ru>
2018-11-07 12:09:25 +0500

    Добавил класс для рисования эллипса

---
+++ Paint/Ellipse.cs
@@ -0,0 +1,25 @@
+﻿namespace Paint
+{
+    using System;
+    using System.Drawing;
+
+    public class Ellipse
+    {
+        private static Point startPosition;
+
+        public static void StartDrawing(Point cursor)
+        {
+            startPosition = cursor;
+        }
+
+        public static void Drawing(Graphics graphics, Pen pen, Point cursor)
+        {
+            graphics.DrawEllipse(
+                pen,
+                Math.Min(startPosition.X, cursor.X),
+                Math.Min(startPosition.Y, cursor.Y),
+                Math.Abs(cursor.X - startPosition.X),
+                Math.Abs(cursor.Y - startPosition.Y));
+        }
+    }
+}
