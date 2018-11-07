f321068be8b1f2400b0345795b45cd4bc8ae2fcc
Максим <kyngfy-panda@yandex.ru>
2018-11-07 12:25:14 +0500

    Добавил класс для открытия файлов

---
+++ Paint/OpenFileSystem.cs
@@ -0,0 +1,23 @@
+﻿namespace Paint
+{
+    using System.Drawing;
+    using System.Windows.Forms;
+
+    public static class OpenFileSystem
+    {
+        public static (string, Image) OpenImage()
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Images|*.GIF;*.TIF;*.JPG;*.BPM";
+            dialog.ShowDialog();
+            if (dialog.FileName == "")
+            {
+                return (string.Empty, null);
+            }
+            else
+            {
+                return (dialog.SafeFileName, Image.FromFile(dialog.FileName));
+            }
+        }
+    }
+}
