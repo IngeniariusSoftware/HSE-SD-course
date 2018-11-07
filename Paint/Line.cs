709018c56370f53e057ac4dbe20f0d2e61332f01
Максим <kyngfy-panda@yandex.ru>
2018-11-07 12:08:57 +0500

    Добавил класс для рисования линии

---
+++ Paint/Line.cs
@@ -0,0 +1,19 @@
+﻿namespace Paint
+{
+    using System.Drawing;
+
+    public static class Line
+    {
+        private static Point startPosition;
+
+        public static void StartDrawing(Point cursor)
+        {
+            startPosition = cursor;
+        }
+
+        public static void Drawing(Graphics graphics, Pen pen, Point cursor)
+        {
+            graphics.DrawLine(pen, startPosition.X, startPosition.Y, cursor.X, cursor.Y);
+        }
+    }
+}
