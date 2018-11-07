2b50dff2818ac753013ffcf01b76109e2c35b5b6
Максим <kyngfy-panda@yandex.ru>
2018-11-07 12:04:55 +0500

    Добавил класс для рисования прямоугольника

---
+++ Paint/Rectangle.cs
@@ -0,0 +1,25 @@
+﻿namespace Paint
+{
+    using System;
+    using System.Drawing;
+
+    public static class Rectangle
+    {
+        private static Point startPosition;
+
+        public static void StartDrawing(Point cursor)
+        {
+            startPosition = cursor;
+        }
+
+        public static void Drawing(Graphics graphics, Pen pen, Point cursor)
+        {
+            graphics.DrawRectangle(
+                pen,
+                Math.Min(startPosition.X, cursor.X),
+                Math.Min(startPosition.Y, cursor.Y),
+                Math.Abs(cursor.X - startPosition.X),
+                Math.Abs(cursor.Y - startPosition.Y));
+        }
+    }
+}
