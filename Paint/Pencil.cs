75876d72092ccf5f26b8edd15c8bda36baa45998
Максим <kyngfy-panda@yandex.ru>
2018-11-07 12:22:14 +0500

    Добавил класс для рисования ручкой

---
+++ Paint/Pencil.cs
@@ -0,0 +1,21 @@
+﻿namespace Paint
+{
+    using System.Drawing;
+
+    public static class Pencil
+    {
+        private static Point startPosition;
+
+        public static void StartDrawing(Graphics graphics, Pen pen, Point cursor)
+        {
+            graphics.DrawLine(pen, startPosition.X, startPosition.Y, startPosition.X, startPosition.Y);
+            startPosition = cursor;
+        }
+
+        public static void Drawing(Graphics graphics, Pen pen, Point cursor)
+        {
+            graphics.DrawLine(pen, startPosition.X, startPosition.Y, cursor.X, cursor.Y);
+            startPosition = cursor;
+        }
+    }
+}
