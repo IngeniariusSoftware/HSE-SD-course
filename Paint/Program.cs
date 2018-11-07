b98c1c00b83099f278fda7489fca2b81af0b9d97
Максим <kyngfy-panda@yandex.ru>
2018-11-07 12:06:38 +0500

    Доработал файл запуска

--- Paint/Program.cs
+++ Paint/Program.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace Paint
+﻿namespace Paint
 {
+    using System.Windows.Forms;
+    using System;
+
     static class Program
     {
         /// <summary>
@@ -16,7 +13,7 @@ namespace Paint
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmMain());
+            Application.Run(new MainForm());
         }
     }
 }
