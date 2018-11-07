8d98c1ebdbff8a391c3600bd98c4b6540c7dd5de
Максим <kyngfy-panda@yandex.ru>
2018-11-07 12:24:31 +0500

    Добавил класс для управления файловой системой

---
+++ Paint/FileNameSystem.cs
@@ -0,0 +1,67 @@
+﻿namespace Paint
+{
+    using System.Collections.Generic;
+
+    public class FileNameSystem
+    {
+        public string LastFile = string.Empty, CurrentFile = string.Empty;
+
+        private List<string> _files = new List<string>();
+
+        public string NewFile()
+        {
+            LastFile = CurrentFile;
+            string newFile = "Безымянный.png";
+            int number = 1;
+            while (_files.Contains(newFile))
+            {
+                newFile = string.Format("Безымянный ({0}).png", number);
+                ++number;
+            }
+
+            _files.Add(newFile);
+            CurrentFile = newFile;
+            return (newFile);
+        }
+
+        public void CloseFile(string fileName)
+        {
+            LastFile = fileName;
+            _files.Remove(fileName);
+            if (_files.Count > 0)
+            {
+                CurrentFile = _files[_files.Count - 1];
+            }
+            else
+            {
+                CurrentFile = string.Empty;
+            }
+        }
+
+        public void AddFile(string fileName)
+        {
+            LastFile = CurrentFile;
+            _files.Add(fileName);
+            CurrentFile = fileName;
+        }
+
+        public void ChangeFile(string fileName)
+        {
+            LastFile = CurrentFile;
+            CurrentFile = fileName;
+        }
+
+        public int GetIndexFile(string fileName)
+        {
+            if (_files.Contains(fileName))
+            {
+                return _files.IndexOf(fileName);
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+    }
+}
