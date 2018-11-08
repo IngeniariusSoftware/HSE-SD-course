21a475ccaae085092b1c51e9e7b5022eb40c5faf
Максим <kyngfy-panda@yandex.ru>
2018-11-08 23:45:18 +0500

    Создал библиотеку для эффектов на изображения

---
+++ LibraryEffects/EffectSystem.cs
@@ -0,0 +1,17 @@
+﻿namespace LibraryEffects
+{
+    using System.Drawing;
+
+    public static class EffectSystem
+    {
+        public static Bitmap Negative(Bitmap image)
+        {
+
+        }
+        
+        public static Bitmap Ыharpness(Bitmap image)
+        {
+
+        }
+    }
+}
