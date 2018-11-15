31a9b66f3fe616260a775a8bec7c3a445b7f217f
Максим <kyngfy-panda@yandex.ru>
2018-11-15 19:23:48 +0500

    Перевел ластик на наследование класса базового инструмента
    Плюс code cleanup

--- Paint/Tools/Eraser.cs
+++ Paint/Tools/Eraser.cs
@@ -1,52 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Paint.Tools
+﻿namespace Paint.Tools
 {
+    using System;
     using System.Drawing;
 
-    public static class Eraser
+    public class Eraser : BaseTool
     {
-        private static Point startPosition;
+        private Bitmap _eraserBoard;
 
-        private static Bitmap _eraserBoard;
-
-        public static void StartDrawing(Image picture, Pen pen, Point cursor)
+        public void StartDrawing(Image picture, Pen pen, Point cursor)
         {
+            _pen = pen;
+            _startPosition = cursor;
             _eraserBoard = (Bitmap)picture;
-            for (int y = (int)Math.Max(startPosition.Y - pen.Width, 0);
-                 y < Math.Min(startPosition.Y + pen.Width, _eraserBoard.Height);
-                 ++y)
-            {
-                for (int x = (int)Math.Max(startPosition.X - pen.Width, 0);
-                     x < Math.Min(startPosition.X + pen.Width, _eraserBoard.Width);
-                     ++x)
-                {
-                    _eraserBoard.SetPixel(x, y, Color.Empty);
-                }
-            }
-
-            startPosition = cursor;
+            Drawing(cursor);
         }
 
-        public static void Drawing(Graphics graphics, Pen pen, Point cursor)
+        public override void Drawing(Point cursor)
         {
-            for (int y = (int)Math.Max(startPosition.Y - pen.Width, 0);
-                 y < Math.Min(startPosition.Y + pen.Width, _eraserBoard.Height);
+            for (int y = (int)Math.Max(_startPosition.Y - _pen.Width, 0);
+                 y < Math.Min(_startPosition.Y + _pen.Width, _eraserBoard.Height);
                  ++y)
             {
-                for (int x = (int)Math.Max(startPosition.X - pen.Width, 0);
-                     x < Math.Min(startPosition.X + pen.Width, _eraserBoard.Width);
+                for (int x = (int)Math.Max(_startPosition.X - _pen.Width, 0);
+                     x < Math.Min(_startPosition.X + _pen.Width, _eraserBoard.Width);
                      ++x)
                 {
                     _eraserBoard.SetPixel(x, y, Color.Empty);
                 }
             }
 
-            startPosition = cursor;
+            _startPosition = cursor;
         }
     }
 }
