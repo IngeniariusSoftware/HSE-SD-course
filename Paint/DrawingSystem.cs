d381bfaa714d825048f03f5eb2188a52e4ecc38c
Максим <kyngfy-panda@yandex.ru>
2018-11-15 19:26:04 +0500

    Перевел систему рисования на использование классов инструментов

--- Paint/DrawingSystem.cs
+++ Paint/DrawingSystem.cs
@@ -3,199 +3,111 @@
     using System.Drawing;
     using System.Drawing.Drawing2D;
     using System.Windows.Forms;
+
     using Paint.Tools;
 
     public static class DrawingSystem
     {
-        public enum Tools
+        private static BaseTool _tool = new Pencil();
+
+        private static Ellipse _ellipse = new Ellipse();
+
+        private static Eraser _eraser = new Eraser();
+
+        private static Line _line = new Line();
+
+        private static Pencil _pencil = new Pencil();
+
+        private static Rectangle _rectangle = new Rectangle();
+
+        private static Star _star = new Star();
+
+        private static BaseTool ToTool(string toolName)
         {
-            Pen,
-
-            Line,
-
-            Rectangle,
-
-            Ellipse,
-
-            Star,
-
-            Eraser,
-
-            LoupePlus,
-
-            LoupeMinus
-        }
-
-        private static Tools _tool = Tools.Pen;
-
-        private static Tools ToTool(string toolName)
-        {
-            Tools tool = Tools.Pen;
+            _tool = _pencil;
             switch (toolName)
             {
                 case "Rectangle":
                     {
-                        tool = Tools.Rectangle;
+                        _tool = _rectangle;
                         break;
                     }
 
                 case "Ellipse":
                     {
-                        tool = Tools.Ellipse;
+                        _tool = _ellipse;
                         break;
                     }
 
                 case "Star":
                     {
-                        tool = Tools.Star;
+                        _tool = _star;
                         break;
                     }
 
                 case "Eraser":
                     {
-                        tool = Tools.Eraser;
+                        _tool = _eraser;
                         break;
                     }
 
                 case "Line":
                     {
-                        tool = Tools.Line;
-                        break;
-                    }
-
-                case "LoupeMinus":
-                    {
-                        tool = Tools.LoupeMinus;
-                        break;
-                    }
-
-                case "LoupePlus":
-                    {
-                        tool = Tools.LoupePlus;
+                        _tool = _line;
                         break;
                     }
             }
 
-            return tool;
+            return _tool;
         }
 
         private static Graphics _graphics;
 
         public static bool IsDrawing;
 
-        public static Bitmap Buffer = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
-
-        private static Size _paintRegionSize;
+        public static Bitmap Buffer;
 
         private static Pen _pen = new Pen(Color.Black, 1);
 
         public static void EndDrawing(Image lastImage)
         {
-            _graphics.Dispose();
+            _graphics?.Dispose();
             _graphics = Graphics.FromImage(lastImage);
-            _graphics.SetClip(new System.Drawing.Rectangle(0, 0, _paintRegionSize.Width, _paintRegionSize.Height));
             _graphics.DrawImage(Buffer, 0, 0);
+            _graphics.Dispose();
             _graphics = Graphics.FromImage(Buffer);
-            _graphics.Clear(Color.Empty);
+            _graphics.Dispose();
+            Buffer.Dispose();
             IsDrawing = false;
         }
 
         public static void StartDrawing(Point cursor, PictureBox pictureBox)
         {
-            _paintRegionSize = pictureBox.Size;
-            if (_tool != Tools.Eraser)
+            Buffer = new Bitmap(pictureBox.Size.Width, pictureBox.Size.Height);
+            if (_tool is Eraser)
+            {
+                ((Eraser)_tool).StartDrawing(pictureBox.BackgroundImage, _pen, cursor);
+            }
+            else
             {
                 pictureBox.Image = Buffer;
                 _graphics = Graphics.FromImage(Buffer);
+                _graphics.CompositingQuality = CompositingQuality.HighQuality;
+                _graphics.SmoothingMode = SmoothingMode.HighQuality;
+                _tool.StartDrawing(_graphics, _pen, cursor);
             }
 
-            _graphics.CompositingQuality = CompositingQuality.HighQuality;
-            _graphics.SmoothingMode = SmoothingMode.HighQuality;
             IsDrawing = true;
-            switch (_tool)
-            {
-                case Tools.Line:
-                    {
-                        Line.StartDrawing(cursor);
-                        break;
-                    }
-
-                case Tools.Rectangle:
-                    {
-                        Rectangle.StartDrawing(cursor);
-                        break;
-                    }
-
-                case Tools.Ellipse:
-                    {
-                        Ellipse.StartDrawing(cursor);
-                        break;
-                    }
-
-                case Tools.Star:
-                    {
-                        Star.StartDrawing(cursor);
-                        break;
-                    }
-
-                case Tools.Pen:
-                    {
-                        Pencil.StartDrawing(_graphics, _pen, cursor);
-                        break;
-                    }
-
-                case Tools.Eraser:
-                    {
-                        Eraser.StartDrawing(pictureBox.BackgroundImage, _pen, cursor);
-                        break;
-                    }
-            }
         }
 
         public static void Drawing(Point currentPosition)
         {
-            switch (_tool)
+            if (!(_tool is Eraser || _tool is Pencil))
             {
-                case Tools.Line:
-                    {
-                        _graphics.Clear(Color.Empty);
-                        Line.Drawing(_graphics, _pen, currentPosition);
-                        break;
-                    }
-
-                case Tools.Rectangle:
-                    {
-                        _graphics.Clear(Color.Empty);
-                        Rectangle.Drawing(_graphics, _pen, currentPosition);
-                        break;
-                    }
-
-                case Tools.Ellipse:
-                    {
-                        _graphics.Clear(Color.Empty);
-                        Ellipse.Drawing(_graphics, _pen, currentPosition);
-                        break;
-                    }
-
-                case Tools.Star:
-                    {
-                        _graphics.Clear(Color.Empty);
-                        Star.Drawing(_graphics, _pen, currentPosition);
-                        break;
-                    }
-
-                case Tools.Pen:
-                    {
-                        Pencil.Drawing(_graphics, _pen, currentPosition);
-                        break;
-                    }
-
-                case Tools.Eraser:
-                    {
-                        Eraser.Drawing(_graphics, _pen, currentPosition);
-                        break;
-                    }
+                _graphics.Clear(Color.Empty);
             }
+
+            _tool.Drawing(currentPosition);
         }
 
         public static void ChangePenSize(float penSize)
@@ -206,17 +118,11 @@
         public static void ChangePenColor(Color newColor)
         {
             _pen.Color = newColor;
-
         }
 
         public static void ChangeTool(string newTool)
         {
             _tool = ToTool(newTool);
-        }
-
-        public static void SetPaintRegionSize(Size size)
-        {
-            _paintRegionSize = size;
         }
     }
 }
