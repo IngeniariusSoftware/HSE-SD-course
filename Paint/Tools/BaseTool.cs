ccb54d442babdb1d0aa3cc49853c5a6c4a812ab3
Максим <kyngfy-panda@yandex.ru>
2018-11-15 19:22:03 +0500

    Доработал абстрактный класс базового инструмента

--- Paint/Tools/BaseTool.cs
+++ Paint/Tools/BaseTool.cs
@@ -1,21 +1,16 @@
 ﻿namespace Paint.Tools
 {
-    using System;
-    using System.Collections.Generic;
     using System.Drawing;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
 
     public abstract class BaseTool
     {
-        private Point _startPosition;
+        protected Point _startPosition;
 
-        private Graphics _graphics;
+        protected Graphics _graphics;
 
-        private Pen _pen;
+        protected Pen _pen;
 
-        public void StartDrawing(Graphics graphics, Pen pen, Point cursor)
+        public virtual void StartDrawing(Graphics graphics, Pen pen, Point cursor)
         {
             _pen = pen;
             _graphics = graphics;
@@ -23,6 +18,5 @@
         }
 
         public abstract void Drawing(Point cursor);
-
     }
 }
