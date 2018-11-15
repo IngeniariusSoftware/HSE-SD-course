7f3c169786558b431bd9c1443622473aad85963e
Максим <kyngfy-panda@yandex.ru>
2018-11-15 19:23:59 +0500

    Перевел линию на наследование класса базового инструмента

--- Paint/Tools/Line.cs
+++ Paint/Tools/Line.cs
@@ -2,18 +2,13 @@
 {
     using System.Drawing;
 
-    public static class Line
+    using Paint.Tools;
+
+    public class Line : BaseTool
     {
-        private static Point startPosition;
-
-        public static void StartDrawing(Point cursor)
+        public override void Drawing(Point cursor)
         {
-            startPosition = cursor;
-        }
-
-        public static void Drawing(Graphics graphics, Pen pen, Point cursor)
-        {
-            graphics.DrawLine(pen, startPosition.X, startPosition.Y, cursor.X, cursor.Y);
+            _graphics.DrawLine(_pen, _startPosition.X, _startPosition.Y, cursor.X, cursor.Y);
         }
     }
 }
