0258189edfd60263ab89cf01f2d7f06e9b74cf0b
Максим <kyngfy-panda@yandex.ru>
2018-11-15 19:24:31 +0500

    Перевел звезду на наследование класса базового инструмента

--- Paint/Tools/Star.cs
+++ Paint/Tools/Star.cs
@@ -3,42 +3,35 @@
     using System;
     using System.Drawing;
 
-    public static class Star
+    public class Star : BaseTool
     {
-        private static Point startPosition;
-
-        public static void StartDrawing(Point cursor)
-        {
-            startPosition = cursor;
-        }
-
-        public static void Drawing(Graphics graphics, Pen pen, Point cursor)
+        public override void Drawing(Point cursor)
         {
             int externalRadius = (int)Math.Sqrt(
-                Math.Pow(startPosition.X - cursor.X, 2) + Math.Pow(startPosition.Y - cursor.Y, 2));
-            int interiorRadius = (int)(externalRadius * 0.2);
+                Math.Pow(_startPosition.X - cursor.X, 2) + Math.Pow(_startPosition.Y - cursor.Y, 2));
+            int interiorRadius = (int)(externalRadius * 0.4);
             int countNodes = 5;
-            double angle = -Math.Atan((double)(startPosition.Y - cursor.Y) / (startPosition.X - cursor.X));
+            double angle = -Math.Atan((double)(_startPosition.Y - cursor.Y) / (_startPosition.X - cursor.X));
             Point[] starPoints = new Point[(countNodes + 1) * 2];
             for (int i = 0; i < countNodes; i++)
             {
                 starPoints[i * 2] = new Point(
-                    x: startPosition.X - (int)(externalRadius * Math.Sin((2.0 * Math.PI / countNodes * i) + angle)),
-                    y: startPosition.Y - (int)(externalRadius * Math.Cos((2.0 * Math.PI / countNodes * i) + angle)));
+                    x: _startPosition.X - (int)(externalRadius * Math.Sin((2.0 * Math.PI / countNodes * i) + angle)),
+                    y: _startPosition.Y - (int)(externalRadius * Math.Cos((2.0 * Math.PI / countNodes * i) + angle)));
                 starPoints[(i * 2) + 1] = new Point(
-                    x: startPosition.X - (int)(interiorRadius
-                                               * Math.Sin((2 * Math.PI / countNodes * i) + (Math.PI / 5.0) + angle)),
-                    y: startPosition.Y - (int)(interiorRadius
-                                               * Math.Cos((2 * Math.PI / countNodes * i) + (Math.PI / 5.0) + angle)));
+                    x: _startPosition.X
+                       - (int)(interiorRadius * Math.Sin((2 * Math.PI / countNodes * i) + (Math.PI / 5.0) + angle)),
+                    y: _startPosition.Y
+                       - (int)(interiorRadius * Math.Cos((2 * Math.PI / countNodes * i) + (Math.PI / 5.0) + angle)));
             }
 
             starPoints[starPoints.Length - 2] = new Point(
-                x: startPosition.X - (int)(externalRadius * Math.Sin(angle)),
-                y: startPosition.Y - (int)(externalRadius * Math.Cos(angle)));
+                x: _startPosition.X - (int)(externalRadius * Math.Sin(angle)),
+                y: _startPosition.Y - (int)(externalRadius * Math.Cos(angle)));
             starPoints[starPoints.Length - 1] = new Point(
-                x: startPosition.X - (int)(interiorRadius * Math.Sin((Math.PI / 5.0) + angle)),
-                y: startPosition.Y - (int)(interiorRadius * Math.Cos((Math.PI / 5.0) + angle)));
-            graphics.DrawLines(pen, starPoints);
+                x: _startPosition.X - (int)(interiorRadius * Math.Sin((Math.PI / 5.0) + angle)),
+                y: _startPosition.Y - (int)(interiorRadius * Math.Cos((Math.PI / 5.0) + angle)));
+            _graphics.DrawLines(_pen, starPoints);
         }
     }
 }
