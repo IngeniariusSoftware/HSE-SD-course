2269014ea3e53ee42f00a9dbd39b2cc58cd6dfb9
Максим <kyngfy-panda@yandex.ru>
2018-11-20 22:59:54 +0500

    Убрал лишние эффекты, инициализировал свои

--- Paint/EffectsSystem.cs
+++ Paint/EffectsSystem.cs
@@ -1,13 +1,10 @@
 ﻿namespace Paint
 {
+    using System.Drawing;
     using AForge.Imaging.Filters;
 
     public static class EffectsSystem
     {
-        public static Mirror MirrorXEffect = new Mirror(true, false);
-
-        public static Mirror MirrorYEffect = new Mirror(false, true);
-
         public static Sepia SepiaEffect = new Sepia();
 
         public static SimpleSkeletonization SimpleSkeletonizationEffect = new SimpleSkeletonization();
@@ -15,5 +12,41 @@
         public static GaussianBlur GaussianBlurEffect = new GaussianBlur();
 
         public static GaussianSharpen GaussianSharpenEffect = new GaussianSharpen();
+
+        public static Negative NegativeEffect = new Negative();
+
+        public static GreenNegative GreenNegativeEffect = new GreenNegative();
+
+        public static Stamping StampingEffect = new Stamping();
+
+        public class Negative
+        {
+            public void ApplyInPlace(Bitmap image)
+            {
+                for (int y = 0; y < image.Height; y++)
+                {
+                    for (int x = 0; x < image.Width; x++)
+                    {
+                        image.SetPixel(x, y, Color.Aqua);
+                    }
+                }
+            }
+        }
+
+        public class GreenNegative
+        {
+            public void ApplyInPlace(Bitmap image)
+            {
+
+            }
+        }
+
+        public class Stamping
+        {
+            public void ApplyInPlace(Bitmap image)
+            {
+
+            }
+        }
     }
 }
