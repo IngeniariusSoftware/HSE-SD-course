61b7949996473675fb5b9d1d6e8f65224dca5efc
Максим <kyngfy-panda@yandex.ru>
2018-11-26 01:28:02 +0400

    Добавил инструмент "Лупа"

---
+++ Paint/Tools/Loupe.cs
@@ -0,0 +1,13 @@
+﻿namespace Paint.Tools
+{
+    using System.Drawing;
+
+    public class Loupe : BaseTool
+    {
+        public bool isIncrease;
+
+        public override void Drawing(Point cursor)
+        {
+        }
+    }
+}
