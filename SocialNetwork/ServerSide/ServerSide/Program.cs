97324704e3074d683d6a5022f345da734f863469
Максим <kyngfy-panda@yandex.ru>
2019-01-07 04:46:12 +0500

    Перенес множество действий в отдельные классы

--- SocialNetwork/ServerSide/ServerSide/Program.cs
+++ SocialNetwork/ServerSide/ServerSide/Program.cs
@@ -8,47 +8,22 @@
     using System.Diagnostics;
     using System.Net;
     using System.Net.Sockets;
+    using System.Threading;
 
     class Program
     {
         private static Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
 
-        static void Main(string[] args)
+        static void Main()
         {
-            socket.Bind(new IPEndPoint(IPAddress.Any, 904));
-            socket.Listen(10000);
-            var buffer = new byte[10500000];
-
-            Console.WriteLine("Сервер запущен. Ожидание подключений...");
-            int messageCount = 1;
-            Stopwatch timer = new Stopwatch();
-
-            while (true)
-            {
-                Socket client = socket.Accept();
-
-                int size;
-                do
-                {
-                    size = client.Receive(buffer);
-                    if (!timer.IsRunning)
-                    {
-                        timer.Start();
-                    }
-                }
-                while (client.Available > 0);
-
-                Console.WriteLine("\nНомер сообщения: {0}", messageCount);
-                Console.WriteLine("Дата сообщения: {0}", DateTime.Now);
-                Console.WriteLine("Отправитель: {0}", client.LocalEndPoint);
-                Console.WriteLine("Сообщение: ");
-                Console.WriteLine(Encoding.ASCII.GetString(buffer, 0, size));
-                Console.WriteLine("Время обработки: {0}", timer.ElapsedMilliseconds / 1000.0);
-                timer.Reset();
-                messageCount++;
-                client.Shutdown(SocketShutdown.Both);
-                client.Close();
-            }
+            Console.Title = "Сервер";
+            Server server = new Server(IPAddress.Any, 904, 10500000, 10000);
+            server.Start();
+            Console.WriteLine("Серверный процесс запущен\n");
+            server.Manage();
+            while (server.IsWorked || server.log.IsWorked) ;
+            Console.WriteLine("\nДля завершения работы нажмите любую клавишу");
+            Console.ReadKey(true);
         }
     }
 }
