9dda2f295a7a89af7b32a2076abc53a52106bda7
Максим <kyngfy-panda@yandex.ru>
2019-01-10 18:59:29 +0500

    Добавил класс наблюдателя за временем и  класс для его событий

---
+++ SocialNetwork/ServerSide/ServerSide/Timing/TimeEventArgs.cs
@@ -0,0 +1,28 @@
+﻿
+namespace ServerSide.Timing
+{
+    using System;
+
+    public delegate void TimeEventHandler(object sender, TimeEventArgs e);
+
+    public class TimeEventArgs : EventArgs
+    {
+        private bool _isNewDay;
+
+        private string _operationInfo;
+
+        public TimeEventArgs(string operationInfo)
+        {
+            _operationInfo = operationInfo;
+        }
+
+        public TimeEventArgs(bool isNewDay)
+        {
+            _isNewDay = isNewDay;
+        }
+
+        public string OperationInfo => _operationInfo;
+
+        public bool IsNewDay => _isNewDay;
+    }
+}
---
+++ SocialNetwork/ServerSide/ServerSide/Timing/TimeObserver.cs
@@ -0,0 +1,64 @@
+﻿
+namespace ServerSide.Timing
+{
+    using System;
+    using System.Threading;
+
+    using ServerSide.Interfaces;
+    using ServerSide.Servicing;
+
+    public class TimeObserver : ITimeObserver, IProcess
+    {
+        private DateTime _timeFrame;
+
+        private bool _isActive;
+
+        private bool _isWorking;
+
+        public TimeObserver()
+        {
+        }
+
+        public event TimeEventHandler ChangeState = delegate { };
+
+        public bool IsActive => _isActive;
+
+        public void Start()
+        {
+            _isActive = true;
+            _timeFrame = DateTime.Today;
+            var timeObserverThread = new Thread(Observe) { Name = "Поток работы таймера" };
+            ChangeState(this, new TimeEventArgs("Таймер запущен"));
+            timeObserverThread.Start();
+        }
+
+        public void End()
+        {
+            _isActive = false;
+            while (_isWorking)
+            {
+                ChangeState(this, new TimeEventArgs("Ожидание завершения работы таймера..."));
+                Thread.Sleep(500);
+            }
+
+            ChangeState(this, new TimeEventArgs("Таймер выключен"));
+        }
+
+        private void Observe()
+        {
+            _isWorking = true;
+            while (_isActive)
+            {
+                if (Math.Abs(_timeFrame.Day - DateTime.Today.Day) > 0)
+                {
+                    _timeFrame = DateTime.Today;
+                    ChangeState(this, new TimeEventArgs(true));
+                }
+
+                Thread.Sleep(1000);
+            }
+
+            _isWorking = false;
+        }
+    }
+}
