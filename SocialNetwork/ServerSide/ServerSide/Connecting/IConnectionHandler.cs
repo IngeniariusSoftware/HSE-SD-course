dd6ce46b8e17593870bc3593dfede5ee1b6032fd
Максим <kyngfy-panda@yandex.ru>
2019-01-18 19:06:36 +0500

    Добавил класс и интерфейс обработчика подключений

---
+++ SocialNetwork/ServerSide/ServerSide/Connecting/IConnectionHandler.cs
@@ -0,0 +1,21 @@
+﻿
+namespace ServerSide.Connecting
+{
+    using System.Net;
+
+    using ServerSide.Interfaces;
+    using ServerSide.Servicing;
+
+    public interface IConnectionHandler : IProcess
+    {
+        event ServerEventHandler OperationHandled;
+
+        bool IsActive { get; }
+
+        double BufferSize { get; }
+
+        int MaxCountConnections { get; }
+
+        IPEndPoint Address { get; }
+    }
+}
