9d913a59c91b3c0c7832249a6e88f06e6d05829e
Максим <kyngfy-panda@yandex.ru>
2019-01-18 19:07:34 +0500

    Добавил перечисления для различных типов полей

---
+++ SocialNetwork/ServerSide/ServerSide/Enumes.cs
@@ -0,0 +1,28 @@
+﻿
+namespace ServerSide
+{
+    public enum MaritalStatus
+    {
+        Single = 0,
+
+        Married = 1
+    }
+
+    public enum Gender
+    {
+        Male = 0,
+
+        Female = 1
+    }
+
+    public enum OperationAction
+    {
+        Add = 0,
+
+        Remove = 1,
+
+        Change = 2,
+
+        IsBirthday = 3
+    }
+}
