123e44ec0afa68206fd5fc86597c07ebeb403a4d
Максим <kyngfy-panda@yandex.ru>
2019-01-21 04:28:02 +0500

    Добавил маппер для преобразования перечислений

---
+++ SocialNetwork/ServerSide/ServerSide/Mapping/Mapper.cs
@@ -0,0 +1,57 @@
+﻿
+namespace ServerSide.Mapping
+{
+    using System;
+
+    public static class Mapper
+    {
+        public static DateTime StringToDate(string data)
+        {
+            string[] tokens = data.Split('.');
+            int day = int.Parse(tokens[0]);
+            int month = int.Parse(tokens[1]);
+            int year = int.Parse(tokens[2]);
+            return new DateTime(year, month, day);
+        }
+
+        public static string GenderToString(string value)
+        {
+            switch (int.Parse(value))
+            {
+                case 0:
+                    {
+                        return "мужской";
+                    }
+                case 1:
+                    {
+                        return "женский";
+                    }
+                default:
+                    {
+                        return "не указан";
+                    }
+            }
+        }
+
+        public static string MaritalToString(string value)
+        {
+            switch (int.Parse(value))
+            {
+                case 0:
+                    {
+                        return "не в браке";
+                    }
+
+                case 1:
+                    {
+                        return "в браке";
+                    }
+
+                default:
+                    {
+                        return "не указан";
+                    }
+            }
+        }
+    }
+}
