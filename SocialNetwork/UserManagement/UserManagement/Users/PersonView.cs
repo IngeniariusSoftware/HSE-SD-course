7f67b560eada8e89ecfebd6298b3c2558062f42f
Максим <kyngfy-panda@yandex.ru>
2019-01-28 21:35:34 +0500

    Добавил облегченный класс пользователя
    Для пересылки только самой необходимой информации о пользователе с сервера на клиент

---
+++ SocialNetwork/UserManagement/UserManagement/Users/PersonView.cs
@@ -0,0 +1,23 @@
+﻿
+namespace UserManagement.Users
+{
+    using System;
+    using System.Drawing;
+
+    [Serializable]
+    public class PersonView
+    {
+        public PersonView(string login, string fullName, Bitmap avatar = null)
+        {
+            Login = login;
+            FullName = fullName;
+            Avatar = avatar;
+        }
+
+        public string Login { get; set; }
+
+        public string FullName { get; set; }
+
+        public Bitmap Avatar { get; set; }
+    }
+}
