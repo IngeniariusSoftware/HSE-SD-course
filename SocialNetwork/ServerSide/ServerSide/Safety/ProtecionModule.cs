e9c0fe9849eaa67e795412c3eae9cb2ca3163812
Максим <kyngfy-panda@yandex.ru>
2019-02-10 22:00:00 +0500

    Добавил модуль безопасности, а также класс сессий пользователя
    1) Модуль безопасности реализует защиту с использованием генерации хэша из пароля и логина, вместо их хранения.
    2) Использование в сообщениях не пароля с логиным, а сгенерированным токеном сессии пользователя при первичной проверки логина и пароля.
    3) Проверка актуальности токена для невозможности использовать посторонними пользователями в дальнейшем.

---
+++ SocialNetwork/ServerSide/ServerSide/Safety/ProtecionModule.cs
@@ -0,0 +1,86 @@
+﻿
+namespace ServerSide.Safety
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Security.Cryptography;
+    using System.Text;
+
+    public class ProtectionModule
+    {
+        private static SHA256 encryptor = SHA256.Create();
+
+        private List<Session> _sessions = new List<Session>();
+
+        public static byte[] GenerateHash(string login, string password)
+        {
+            byte[] data = Encoding.UTF8.GetBytes(login + password);
+            return encryptor.ComputeHash(data);
+        }
+
+        public bool CheckSession(string userLogin, Guid token)
+        {
+            Session userSession = _sessions.Find(x => x.Login == userLogin && x.Token == token);
+            if (userSession != null)
+            {
+                if ((DateTime.Now - userSession.LastUpdate).Minutes < 60)
+                {
+                    userSession.LastUpdate = DateTime.Now;
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public Guid UpdateSession(string userLogin, string password)
+        {
+            Session userSession = _sessions.Find(x => x.Login == userLogin);
+            if (userSession != null)
+            {
+                bool isRight = true;
+                byte[] key = GenerateHash(userLogin, password);
+                for (int i = 0; i < key.Length && isRight; i++)
+                {
+                    if (key[i] != userSession.ProtectKey[i])
+                    {
+                        isRight = false;
+                    }
+                }
+
+                if (isRight)
+                {
+                    userSession.LastUpdate = DateTime.Now;
+                    userSession.Token = Guid.NewGuid();
+                    return userSession.Token;
+                }
+                else
+                {
+                    return Guid.Empty;
+                }
+            }
+            else
+            {
+                return Guid.Empty;
+            }
+        }
+
+        public Guid AddSession(string userLogin, string password)
+        {
+            Session userSession = _sessions.Find(x => x.Login == userLogin);
+            if (userSession == null)
+            {
+                userSession = new Session(userLogin, password);
+                _sessions.Add(userSession);
+            }
+
+            return userSession.Token;
+        }
+    }
+}
---
+++ SocialNetwork/ServerSide/ServerSide/Safety/Session.cs
@@ -0,0 +1,24 @@
+﻿
+namespace ServerSide.Safety
+{
+    using System;
+
+    public class Session
+    {
+        public Session(string login, string password)
+        {
+            Login = login;
+            ProtectKey = ProtectionModule.GenerateHash(login, password);
+            Token = Guid.NewGuid();
+            LastUpdate = DateTime.Now;
+        }
+
+        public string Login { get; }
+
+        public byte[] ProtectKey { get; }
+
+        public Guid Token { get; set; }
+
+        public DateTime LastUpdate { get; set; }
+    }
+}
