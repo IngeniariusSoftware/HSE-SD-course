565c9150d1ea4f96f87a89ef21af33fb567cad18
Максим <kyngfy-panda@yandex.ru>
2019-02-15 22:06:14 +0500

    Настройки проекта сервера

--- SocialNetwork/ServerSide/ServerSide/Properties/AssemblyInfo.cs
+++ SocialNetwork/ServerSide/ServerSide/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
-[assembly: AssemblyTitle("ServerSide")]
+[assembly: AssemblyTitle("Ser")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ServerSide")]
+[assembly: AssemblyProduct("Ser")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
-[assembly: Guid("810cd101-b101-4b9c-b92e-0f44ff02b76c")]
+[assembly: Guid("7fcdaae5-1d6d-449f-97bf-1f075b2cbee8")]
 
 // Сведения о версии сборки состоят из следующих четырех значений:
 //
