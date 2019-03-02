6ad6bd634111eefe42db5bf5ef236e573602d2bb
Максим <kyngfy-panda@yandex.ru>
2019-03-02 22:02:55 +0500

    Добавил классы для организаций

---
+++ SerializationXPath/SerializationXPath/Customer.cs
@@ -0,0 +1,49 @@
+﻿
+namespace SerializationXPath
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Customer
+    {
+        public string FullName;
+
+        public string INN;
+
+        public string KPP;
+
+        public List<Position> Positions = new List<Position>();
+
+        public Customer()
+        {
+        }
+
+        public Customer(string fullName, string inn, string kpp)
+        {
+            FullName = fullName;
+            INN = inn;
+            KPP = kpp;
+        }
+
+        public int PositionCount;
+
+        public double Average(string prefix)
+        {
+            int positionCount = Positions.Count(x => x.Code != null && x.Code.StartsWith(prefix));
+            if (positionCount != 0)
+            {
+                return Positions.Where(x => x.Code != null && x.Code.StartsWith(prefix)).Sum(x => x.Cost)
+                       / positionCount;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public override string ToString()
+        {
+            return FullName;
+        }
+    }
+}
---
+++ SerializationXPath/SerializationXPath/Position.cs
@@ -0,0 +1,15 @@
+﻿namespace SerializationXPath
+{
+    public class Position
+    {
+        public Position(string code, double cost)
+        {
+            Code = code;
+            Cost = cost;
+        }
+
+        public string Code;
+
+        public double Cost;
+    }
+}
