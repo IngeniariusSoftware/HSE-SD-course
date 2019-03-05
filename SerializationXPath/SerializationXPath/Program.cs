3e81b58fc0252d2a722e9d1026598e248d34eab5
Максим <kyngfy-panda@yandex.ru>
2019-03-05 23:31:45 +0500

    Изменил логику работы проекта с сериализацией
    Изменил под новую логику работы с отдельными классами

--- SerializationXPath/SerializationXPath/Program.cs
+++ SerializationXPath/SerializationXPath/Program.cs
@@ -4,12 +4,9 @@ namespace SerializationXPath
     using System;
     using System.Collections.Generic;
     using System.IO;
-    using System.Linq;
     using System.Runtime.Serialization.Formatters.Binary;
     using System.Xml;
     using System.Xml.Serialization;
-
-    using Ionic.Zip;
 
     using Tree_identification;
 
@@ -17,19 +14,20 @@ namespace SerializationXPath
     {
         public static void Main()
         {
-            var tree = new BinaryTree();
-            foreach (string sentence in TreeMaker.FileParse("тест"))
-            {
-                tree.Add(TreeMaker.SentenceParse(sentence));
-            }
-
-            var binaryTree = (BinaryTree)TestTreeBinarySerialize(tree);
-            var xmlTree = (BinaryTree)TestTreeXmlSerialize(tree);
+            MakeTreeSerializationTask();
 
             MakeXmlTask();
 
             Console.WriteLine("\n\tДля завершения работы нажмите любую клавишу...");
             Console.ReadKey();
+        }
+
+        public static void MakeTreeSerializationTask()
+        {
+            string treePath = "тест";
+            var tree = new BinaryTree(treePath);
+            var binaryTree = (BinaryTree)TestTreeBinarySerialize(tree);
+            var xmlTree = (BinaryTree)TestTreeXmlSerialize(tree);
         }
 
         private static object TestTreeBinarySerialize(object element)
@@ -75,10 +73,10 @@ namespace SerializationXPath
         private static void MakeXmlTask()
         {
             var customers = new List<Customer>();
+            string path = null;
+            string outPath = ZipWorker.ExtractZipFiles(path);
 
-            ExtractZipFile(out string path);
-
-            foreach (var file in Directory.GetFiles(path, "*xml"))
+            foreach (var file in Directory.GetFiles(outPath, "*xml"))
             {
                 foreach (Customer customer in ParseXmlFile(file))
                 {
@@ -91,44 +89,10 @@ namespace SerializationXPath
 
                     existCustomer.PositionCount += customer.PositionCount;
                     existCustomer.Positions.AddRange(customer.Positions);
-
-                    //foreach (var position in customer.Positions)
-                    //{
-                    //    var existPosition = existCustomer.Positions.Find(x => x.Code == position.Code);
-                    //    if (existPosition != null)
-                    //    {
-                    //        existPosition.Cost += position.Cost;
-                    //    }
-                    //    else
-                    //    {
-                    //        existCustomer.Positions.Add(position);
-                    //    }
-                    //}
                 }
             }
 
             PrintCustomers(customers);
-        }
-
-        private static void ExtractZipFile(out string outputPath)
-        {
-            do
-            {
-                outputPath = Guid.NewGuid().ToString();
-            }
-            while (Directory.Exists(outputPath));
-
-            Directory.CreateDirectory(outputPath);
-
-            foreach (string zipFile in Directory.GetFiles(Directory.GetCurrentDirectory(), "*zip"))
-            {
-                using (var zip = ZipFile.Read(zipFile))
-                {
-                    zip.ExtractAll(outputPath);
-                }
-            }
-
-            Console.WriteLine($"\n\tФайлы распакованы в папку {outputPath}");
         }
 
         private static List<Customer> ParseXmlFile(string path)
@@ -144,24 +108,25 @@ namespace SerializationXPath
                 {
                     var customer = new Customer
                                        {
-                                           FullName = FindXmlNode(tenderPlan, "commonInfo/customerInfo/fullName")
+                                           FullName =
+                                               XmlWorker.FindXmlNode(tenderPlan, "commonInfo/customerInfo/fullName")
+                                                   ?.FirstChild?.Value,
+                                           INN = XmlWorker.FindXmlNode(tenderPlan, "commonInfo/customerInfo/INN")
                                                ?.FirstChild?.Value,
-                                           INN = FindXmlNode(tenderPlan, "commonInfo/customerInfo/INN")?.FirstChild
-                                               ?.Value,
-                                           KPP = FindXmlNode(tenderPlan, "commonInfo/customerInfo/KPP")?.FirstChild
-                                               ?.Value
+                                           KPP = XmlWorker.FindXmlNode(tenderPlan, "commonInfo/customerInfo/KPP")
+                                               ?.FirstChild?.Value
                                        };
 
-                    XmlNode positions = FindXmlNode(tenderPlan, "positions");
+                    XmlNode positions = XmlWorker.FindXmlNode(tenderPlan, "positions");
                     if (positions != null)
                     {
                         foreach (XmlElement position in positions)
                         {
-                            string code = FindXmlNode(position, "purchaseObjectInfo/OKPD2Info/OKPD2/code")?.FirstChild
-                                ?.Value;
+                            string code = XmlWorker.FindXmlNode(position, "purchaseObjectInfo/OKPD2Info/OKPD2/code")
+                                ?.FirstChild?.Value;
                             double cost;
                             if (double.TryParse(
-                                FindXmlNode(position, "commonInfo/financeInfo/planPayments/total")?.FirstChild
+                                XmlWorker.FindXmlNode(position, "commonInfo/financeInfo/planPayments/total")?.FirstChild
                                     ?.Value,
                                 out cost))
                             {
@@ -183,61 +148,13 @@ namespace SerializationXPath
             return customers;
         }
 
-        private static XmlNode FindXmlNode(XmlNode root, string path)
-        {
-            XmlNode goalRoot = root.Clone();
-            bool isFound = true;
-            string[] nodes = path.Split('/');
-
-            for (int i = 0; i < nodes.Length && isFound; i++)
-            {
-                isFound = false;
-                for (int j = 0; j < goalRoot.ChildNodes.Count && !isFound; j++)
-                {
-                    if (goalRoot.ChildNodes[j].Name == nodes[i])
-                    {
-                        goalRoot = goalRoot.ChildNodes[j];
-                        isFound = true;
-                    }
-                }
-            }
-
-            if (isFound)
-            {
-                return goalRoot;
-            }
-            else
-            {
-                return null;
-            }
-        }
-
         private static void PrintCustomers(List<Customer> customers)
         {
             string specialCode = "63";
             foreach (var customer in customers)
             {
-                Console.WriteLine($"\nОрганизация: {customer}");
-                Console.WriteLine($"ИНН: {customer.INN}");
-                Console.WriteLine($"КПП: {customer.KPP}");
+                Console.Write(customer);
                 Console.WriteLine($"Среднее арифметическое для кода {specialCode}: {customer.Average(specialCode)}");
-                Console.WriteLine($"Количество позиций: {customer.PositionCount}");
-
-                //Console.WriteLine("Позиции:");
-
-                //foreach (var customerPosition in customer.Positions.Where(
-                //    x => x.Code != null && x.Code.StartsWith(specialCode)))
-                //{
-                //    Console.WriteLine($"\tКод: {customerPosition.Code}");
-                //    Console.WriteLine($"\tЦена: {customerPosition.Cost}\n");
-                //}
-
-                //foreach (var customerPosition in customer.Positions.Where(
-                //    x => x.Code == null || !x.Code.StartsWith(specialCode)))
-                //{
-                //    Console.WriteLine($"\tКод: {customerPosition.Code}");
-                //    Console.WriteLine($"\tЦена: {customerPosition.Cost}\n");
-                //}
             }
         }
     }
