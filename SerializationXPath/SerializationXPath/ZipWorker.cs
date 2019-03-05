85cf3af5ce245c2026fe07d8a93879cd6b76a537
Максим <kyngfy-panda@yandex.ru>
2019-03-05 23:30:07 +0500

    Вынес логику работы с zip-файлами в отдельный класс

---
+++ SerializationXPath/SerializationXPath/ZipWorker.cs
@@ -0,0 +1,36 @@
+﻿
+namespace SerializationXPath
+{
+    using System;
+    using System.IO;
+
+    using Ionic.Zip;
+
+    public static class ZipWorker
+    {
+        public static string ExtractZipFiles(string path)
+        {
+            string outputPath;
+            do
+            {
+                outputPath = Guid.NewGuid().ToString();
+            }
+            while (Directory.Exists(outputPath));
+
+            Directory.CreateDirectory(outputPath);
+
+            path = path ?? Directory.GetCurrentDirectory();
+
+            foreach (string zipFile in Directory.GetFiles(path, "*zip"))
+            {
+                using (var zip = ZipFile.Read(zipFile))
+                {
+                    zip.ExtractAll(outputPath);
+                }
+            }
+
+            Console.WriteLine($"\n\tФайлы распакованы в папку {outputPath}");
+            return outputPath;
+        }
+    }
+}
